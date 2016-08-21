28d248ed0a3d04874b4bcfbe897b51af559492ff
lyplay <i@lyplay.com>
2016-08-21 09:35:43 +0800

    toastr alert message update
    Change-Id: I6816993b5d678cc8b87c8f58ec5930b56502b006

--- sflow-web/src/main/webapp/js/scripts/common/alert.js
+++ sflow-web/src/main/webapp/js/scripts/common/alert.js
@@ -1,30 +1,74 @@
-var allM = "asdf";
-var alert = function(){
+var alert = (function(){
 	
-	return "sdfaf";
+	return (function(){
+		
+		var alert = {
+			topCenter:topCenter,
+			topFullWidth:topFullWidth,
+			topRight:topRight,
+			topLeft:topLeft,
+			bottomCenter:bottomCenter,
+			bottomFullWidth:bottomFullWidth,
+			bottomRight:bottomRight,
+			bottomLeft:bottomLeft
+		};
+		
+		return alert;
+		
+		/////////
+		function getToastr(positionClass,closeButton){
+			positionClass = positionClass || "toast-bottom-center";
+			closeButton = closeButton || false;
+			toastr.options = {
+		        "closeButton": closeButton, //是否显示关闭按钮
+		        "debug": false, //是否使用debug模式
+		        "positionClass": positionClass,//弹出窗的位置
+		        "showDuration": "300",//显示的动画时间
+		        "hideDuration": "300",//消失的动画时间
+		        "timeOut": "3000", //展现时间
+		        "extendedTimeOut": "1000",//加长展示时间
+		        "showEasing": "swing",//显示时的动画缓冲方式
+		        "hideEasing": "linear",//消失时的动画缓冲方式
+		        "showMethod": "fadeIn",//显示时的动画方式
+		        "hideMethod": "fadeOut" //消失时的动画方式
+			};
+			return toastr;
+		}
+		
+		function topCenter(closeButton){
+			return getToastr("toast-top-center",closeButton);
+		};
+		
+		function topFullWidth(closeButton){
+			return getToastr("toast-top-full-width",closeButton);
+		};
+		
+		function topRight(closeButton){
+			return getToastr("toast-top-right",closeButton);
+		};
+		
+		function topLeft(closeButton){
+			return getToastr("toast-top-left",closeButton);
+		};
+		
+		function bottomCenter(closeButton){
+			return getToastr("toast-bottom-center",closeButton);
+		};
+		
+		function bottomFullWidth(closeButton){
+			return getToastr("toast-bottom-full-width",closeButton);
+		};
+		
+		function bottomRight(closeButton){
+			return getToastr("toast-bottom-right",closeButton);
+		};
+		
+		function bottomLeft(closeButton){
+			return getToastr("toast-bottom-left",closeButton);
+		};
+		
+	})();
 	
+//	return new Alert();
 	
-};
-
-function alertSuccess(msg){
-	toastr.success(msg);
-}
-
-function alertSuccess2(msg){
-	toastr.clear();
-	toastr.options = {
-	        "closeButton": true, //是否显示关闭按钮
-	        "debug": false, //是否使用debug模式
-	        "positionClass": "toast-top-full-width",//弹出窗的位置
-	        "showDuration": "300",//显示的动画时间
-	        "hideDuration": "1000",//消失的动画时间
-	        "timeOut": "5000", //展现时间
-	        "extendedTimeOut": "1000",//加长展示时间
-	        "showEasing": "swing",//显示时的动画缓冲方式
-	        "hideEasing": "linear",//消失时的动画缓冲方式
-	        "showMethod": "fadeIn",//显示时的动画方式
-	        "hideMethod": "fadeOut" //消失时的动画方式
-	        };
-	toastr.success(msg);
-}
-
+})();
--- sflow-web/src/main/webapp/js/scripts/controllers/controllers.js
+++ sflow-web/src/main/webapp/js/scripts/controllers/controllers.js
@@ -6,9 +6,9 @@ sFlowCtrls.controller("loginCtrl", function($scope,$http) {
     $scope.login = function(){
     	$http({method:'POST',url:'api/login',params:$scope.user}).success(function(response) {  
         	if(response.success){
-        		toastr.success("success");
+        		alert.topCenter(false).success("success. 这里接着往下写，如果控制器的数量非常多，需要分给多个开发者，可以借助于grunt来合并代码");
         	}else{
-        		toastr.error("failed");
+        		alert.topCenter().error("failed. 这里接着往下写，如果控制器的数量非常多，需要分给多个开发者，可以借助于grunt来合并代码");
         		
         	}
      	}); 
