7cd4fbcf6168929992163179a1b435fe12cff1a9
lyplay <i@lyplay.com>
2016-09-01 23:44:25 +0800

    codes upload
    Change-Id: Ia5b35febd6707131c0a117b26d0a493b052b2fe5

--- sflow-api/src/main/java/com/lyplay/sflow/mvc/config/PatchcaController.java
+++ sflow-api/src/main/java/com/lyplay/sflow/mvc/config/PatchcaController.java
@@ -23,61 +23,61 @@ public class PatchcaController {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	private static ConfigurableCaptchaService cs = new ConfigurableCaptchaService();
-	private static Random random = new Random();
-
-	static {
-		cs.setColorFactory(new ColorFactory() {
-			@Override
-			public Color getColor(int x) {
-				int[] c = new int[3];
-				int i = random.nextInt(c.length);
-				for (int fi = 0; fi < c.length; fi++) {
-					if (fi == i) {
-						c[fi] = random.nextInt(71);
-					} else {
-						c[fi] = random.nextInt(256);
-					}
-				}
-				return new Color(c[0], c[1], c[2]);
-			}
-		});
-
-		RandomWordFactory wf = new RandomWordFactory();
-		ResourceBundle rb = ResourceBundle.getBundle("system");
-		wf.setCharacters(rb.getString("captcha.characters"));
-		wf.setMaxLength(Integer.parseInt(rb.getString("captcha.length")));
-		wf.setMinLength(Integer.parseInt(rb.getString("captcha.length")));
-		cs.setWordFactory(wf);
-		cs.setWidth(Integer.parseInt(rb.getString("captcha.width")));
-		cs.setHeight(Integer.parseInt(rb.getString("captcha.height")));
-
-		List<BufferedImageOp> filters = new ArrayList<BufferedImageOp>();
-		filters.add(new SoftenImageOp());
-		ConfigurableFilterFactory cf = new ConfigurableFilterFactory();
-		cf.setFilters(filters);
-		cs.setFilterFactory(cf);
-	}
-
-	@RequestMapping(params = "method=getpatchca")
-	public void getCaptcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		HttpSession session = request.getSession(false);
-		if (session == null) {
-			session = request.getSession();
-		}
-		setResponseHeaders(response);
-		String token = EncoderHelper.getChallangeAndWriteImage(cs, "png", response.getOutputStream());
-		session.setAttribute("captchaToken", token);
-		System.out.println("SessionID=" + session.getId() + "，captcha=" + token);
-	}
-
-	protected void setResponseHeaders(HttpServletResponse response) {
-		response.setContentType("image/png");
-		response.setHeader("Cache-Control", "no-cache, no-store");
-		response.setHeader("Pragma", "no-cache");
-		long time = System.currentTimeMillis();
-		response.setDateHeader("Last-Modified", time);
-		response.setDateHeader("Date", time);
-		response.setDateHeader("Expires", time);
-	}
+//	private static ConfigurableCaptchaService cs = new ConfigurableCaptchaService();
+//	private static Random random = new Random();
+//
+//	static {
+//		cs.setColorFactory(new ColorFactory() {
+//			@Override
+//			public Color getColor(int x) {
+//				int[] c = new int[3];
+//				int i = random.nextInt(c.length);
+//				for (int fi = 0; fi < c.length; fi++) {
+//					if (fi == i) {
+//						c[fi] = random.nextInt(71);
+//					} else {
+//						c[fi] = random.nextInt(256);
+//					}
+//				}
+//				return new Color(c[0], c[1], c[2]);
+//			}
+//		});
+//
+//		RandomWordFactory wf = new RandomWordFactory();
+//		ResourceBundle rb = ResourceBundle.getBundle("system");
+//		wf.setCharacters(rb.getString("captcha.characters"));
+//		wf.setMaxLength(Integer.parseInt(rb.getString("captcha.length")));
+//		wf.setMinLength(Integer.parseInt(rb.getString("captcha.length")));
+//		cs.setWordFactory(wf);
+//		cs.setWidth(Integer.parseInt(rb.getString("captcha.width")));
+//		cs.setHeight(Integer.parseInt(rb.getString("captcha.height")));
+//
+//		List<BufferedImageOp> filters = new ArrayList<BufferedImageOp>();
+//		filters.add(new SoftenImageOp());
+//		ConfigurableFilterFactory cf = new ConfigurableFilterFactory();
+//		cf.setFilters(filters);
+//		cs.setFilterFactory(cf);
+//	}
+//
+//	@RequestMapping(params = "method=getpatchca")
+//	public void getCaptcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//		HttpSession session = request.getSession(false);
+//		if (session == null) {
+//			session = request.getSession();
+//		}
+//		setResponseHeaders(response);
+//		String token = EncoderHelper.getChallangeAndWriteImage(cs, "png", response.getOutputStream());
+//		session.setAttribute("captchaToken", token);
+//		System.out.println("SessionID=" + session.getId() + "，captcha=" + token);
+//	}
+//
+//	protected void setResponseHeaders(HttpServletResponse response) {
+//		response.setContentType("image/png");
+//		response.setHeader("Cache-Control", "no-cache, no-store");
+//		response.setHeader("Pragma", "no-cache");
+//		long time = System.currentTimeMillis();
+//		response.setDateHeader("Last-Modified", time);
+//		response.setDateHeader("Date", time);
+//		response.setDateHeader("Expires", time);
+//	}
 }
---
+++ sflow-service/src/main/java/com/lyplay/sflow/po/Role.java
@@ -0,0 +1,14 @@
+package com.lyplay.sflow.po;
+
+import java.io.Serializable;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="sflow_role")
+public class Role implements Serializable{
+	
+	
+	
+	
+}
