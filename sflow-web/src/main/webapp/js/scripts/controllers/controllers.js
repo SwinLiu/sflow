54488f1a26dee5d7a278bd6be9de3a65caf2b8c0
lyplay <i@lyplay.com>
2016-11-02 00:16:26 +0800

    register page update
    Change-Id: I4b1b21b81bbeb9c8429628338842ad4a3af1daaa

--- sflow-web/src/main/webapp/js/scripts/controllers/controllers.js
+++ sflow-web/src/main/webapp/js/scripts/controllers/controllers.js
@@ -13,12 +13,10 @@ sFlowCtrls.controller("loginCtrl", function($scope,$http) {
 		imgSrc = imgSrc.substring(0, realLength > 0 ? realLength : imgSrc.length); 
 		imgSrc = imgSrc + "?random=" + Math.random();
 		$(this).attr("src",imgSrc);
-		console.log($scope.user);
 	});
 	
 	$("#login_btn").unbind("click").click(function(){
-		if(validator.form()){
-	    	//通过验证后运行的函数，里面要加上表单提交的函数，否则表单不会提交。
+		if($scope.validator.form()){
 	        $http({method:'POST',url:'api/login',data:$scope.user}).success(function(response) {  
 	        	if(response.success){
 	        		alert.topCenter(true).success("success.");
@@ -42,7 +40,7 @@ sFlowCtrls.controller("loginCtrl", function($scope,$http) {
 		}
 	});
 		
-	var validator = $("#login-frm").validate({
+	$scope.validator = $("#login-frm").validate({
         rules: {
         	loginAccount: {
                 required: true,
@@ -74,24 +72,10 @@ sFlowCtrls.controller("loginCtrl", function($scope,$http) {
         success: 'valid',
         focusInvalid:false,//提交表单后，未通过验证的表单（第一个或提交之前获得焦点的未通过验证的表单）会获得焦点
         focusCleanup:false,//当未通过验证的元素获得焦点时，移除错误提示（避免和 focusInvalid 一起使用）
-//        unhighlight: function (element, errorClass, validClass) { 
-//        	//验证通过
-//        },
-//        highlight: function (element, errorClass, validClass) { 
-//        	//未通过验证
-//        },
-//        errorPlacement: function (errorLabel, element) {
-//        	//指明错误放置的位置，默认情况是：error.appendTo(element.parent());即把错误信息放在验证的元素后面。
-//        	msgDiv.error("#login-msg-area",errorLabel.html(),true);
-//			$("#login-msg-area").removeClass('hidden');
-//        },
         errorContainer: "#login-msg-area",
-        errorLabelContainer: $("#login-msg-div")//,
-        //errorElement:"li",
-        //wrapper:"ul",
-//        submitHandler: function (form) {
-//        	form.submit();
-//        }
+        errorLabelContainer: $("#login-msg-div"),
+        errorElement:"li",
+        wrapper:"ul"
     });
     
 
@@ -99,26 +83,51 @@ sFlowCtrls.controller("loginCtrl", function($scope,$http) {
 
 
 /**
- * Login page controller method
+ * register page controller method
  */
 sFlowCtrls.controller("registerCtrl", function($scope,$http) {
 	
-	$scope.user = {userName: "", email: "", phone: ""};
-	$scope.password = "";
+	$scope.user = {userName: "", password: "", email: "", phone: "", captchaCode: ""};
 	
-	$("#register-frm").validate({
+	$("#register_btn").unbind("click").click(function(){
+		if($scope.validator.form()){
+			console.log($scope.user);
+	        $http({method:'POST',url:'api/register',data:$scope.user}).success(function(response) {  
+	        	if(response.success){
+	        		alert.topCenter(true).success("success.");
+	        	}else{
+	        		
+	        		if(response.message == "ERR_01"){
+	        			var msg = 'Captcha Code Error.';
+	            		msgDiv.error("#login-msg-area",null,msg,true);
+						$("#login-msg-area").show();
+	        		}else{
+	        			var msg = 'Login account or password error.';
+	            		msgDiv.error("#login-msg-area",null,msg,true);
+						$("#login-msg-area").show();
+						
+						$("#captchaCodeImg").click();
+						$scope.user.captchaCode = "";
+						
+	        		}
+	        	}
+	     	});
+		}
+	});
+	
+	$scope.validator = $("#register-frm").validate({
         rules: {
         	userName: {
                 required: true,
-                maxlength:40
+                rangelength : [4,40]
             },
-            password: {
+            passwd: {
             	required : true,
             	rangelength : [6,20]
             },
             passwdConfirm:{
             	required : true,
-            	equalsTo : "#password"
+            	equalsTo : "#passwd"
             },
             email:{
             	required : true,
@@ -126,33 +135,50 @@ sFlowCtrls.controller("registerCtrl", function($scope,$http) {
             },
             phone:{
             	maxlength:20
-            }
+            },
+            captchaCode: {
+            	required : true
+            },
+            agree: {
+            	required : true
+            },
         },
         messages: {
         	userName: {
+        		required: "User Name cann't be null. ",
+        		rangelength: $.validator.format( "Please enter a value between {0} and {1} characters long." )
             },
-            password: {
+            passwd: {
+            	required: "Password cann't be null. ",
+            	rangelength: $.validator.format( "Please enter a value between {0} and {1} characters long." )
             },
             passwdConfirm:{
+            	required: "Confirm password cann't be null. ",
+            	equalTo: "Please enter the same password again. "
             },
             email:{
+            	required: "Email cann't be null. "
             },
-            phone:{
-            }
+            captchaCode:{
+            	required: "Captcha Code cann't be null. "
+            },
+            agree:{
+            	required: "Please agree the Terms of Service and Privacy Policy. "
+            },
         },
         onkeyup:false,
         onclick:false,
         onfocusout:false,
         errorClass: "error",
         success: 'valid',
-        focusInvalid:false,//提交表单后，未通过验证的表单（第一个或提交之前获得焦点的未通过验证的表单）会获得焦点
-        focusCleanup:true,//当未通过验证的元素获得焦点时，移除错误提示（避免和 focusInvalid 一起使用）
-        errorContainer: "#login-msg-area",
-        errorLabelContainer: $("#login-msg-div"),
+        focusInvalid:false,
+        focusCleanup:false,
+        errorContainer: "#register-msg-area",
+        errorLabelContainer: $("#register-msg-div"),
         errorElement:"li",
         wrapper:"ul",
-        submitHandler: function (form) {
-        	
+        success: function(label) {
+            label.parent().remove();
         }
     });
 	
