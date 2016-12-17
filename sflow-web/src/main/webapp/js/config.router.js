d663598203e6a8d22d8c6ab0abd0939e3f826d0a
lyplay <i@lyplay.com>
2016-12-18 07:22:11 +0800

    new template upload
    Change-Id: I8495223e56a3e6c6e09eaf5800dad01936e100b3

---
+++ sflow-web/src/main/webapp/js/config.router.js
@@ -0,0 +1,124 @@
+'use strict';
+
+/**
+ * Config for the router
+ */
+angular.module('app')
+  .run(
+    [          '$rootScope', '$state', '$stateParams',
+      function ($rootScope,   $state,   $stateParams) {
+          $rootScope.$state = $state;
+          $rootScope.$stateParams = $stateParams;        
+      }
+    ]
+  )
+  .config(
+    [          '$stateProvider', '$urlRouterProvider',
+      function ($stateProvider,   $urlRouterProvider) {
+          
+          $urlRouterProvider
+              .otherwise('/app/dashboard-v1');
+          $stateProvider
+              .state('app', {
+                  abstract: true,
+                  url: '/app',
+                  templateUrl: 'tpl/app.html'
+              })
+              .state('app.dashboard-v1', {
+                  url: '/dashboard-v1',
+                  templateUrl: 'tpl/app_dashboard_v1.html',
+                  resolve: {
+                    deps: ['$ocLazyLoad',
+                      function( $ocLazyLoad ){
+                        return $ocLazyLoad.load(['js/controllers/chart.js']);
+                    }]
+                  }
+              })
+              .state('app.dashboard-v2', {
+                  url: '/dashboard-v2',
+                  templateUrl: 'tpl/app_dashboard_v2.html',
+                  resolve: {
+                    deps: ['$ocLazyLoad',
+                      function( $ocLazyLoad ){
+                        return $ocLazyLoad.load(['js/controllers/chart.js']);
+                    }]
+                  }
+              })
+
+              // fullCalendar
+              .state('app.calendar', {
+                  url: '/calendar',
+                  templateUrl: 'tpl/app_calendar.html',
+                  // use resolve to load other dependences
+                  resolve: {
+                      deps: ['$ocLazyLoad', 'uiLoad',
+                        function( $ocLazyLoad, uiLoad ){
+                          return uiLoad.load(
+                            ['vendor/jquery/fullcalendar/fullcalendar.css',
+                              'vendor/jquery/fullcalendar/theme.css',
+                              'vendor/jquery/jquery-ui-1.10.3.custom.min.js',
+                              'vendor/libs/moment.min.js',
+                              'vendor/jquery/fullcalendar/fullcalendar.min.js',
+                              'js/app/calendar/calendar.js']
+                          ).then(
+                            function(){
+                              return $ocLazyLoad.load('ui.calendar');
+                            }
+                          )
+                      }]
+                  }
+              })
+
+              .state('layout', {
+                  abstract: true,
+                  url: '/layout',
+                  templateUrl: 'tpl/layout.html'
+              })
+              .state('layout.fullwidth', {
+                  url: '/fullwidth',
+                  views: {
+                      '': {
+                          templateUrl: 'tpl/layout_fullwidth.html'
+                      },
+                      'footer': {
+                          templateUrl: 'tpl/layout_footer_fullwidth.html'
+                      }
+                  },
+                  resolve: {
+                      deps: ['uiLoad',
+                        function( uiLoad ){
+                          return uiLoad.load( ['js/controllers/vectormap.js'] );
+                      }]
+                  }
+              })
+              .state('layout.mobile', {
+                  url: '/mobile',
+                  views: {
+                      '': {
+                          templateUrl: 'tpl/layout_mobile.html'
+                      },
+                      'footer': {
+                          templateUrl: 'tpl/layout_footer_mobile.html'
+                      }
+                  }
+              })
+              .state('layout.app', {
+                  url: '/app',
+                  views: {
+                      '': {
+                          templateUrl: 'tpl/layout_app.html'
+                      },
+                      'footer': {
+                          templateUrl: 'tpl/layout_footer_fullwidth.html'
+                      }
+                  },
+                  resolve: {
+                      deps: ['uiLoad',
+                        function( uiLoad ){
+                          return uiLoad.load( ['js/controllers/tab.js'] );
+                      }]
+                  }
+              })
+      }
+    ]
+  );
